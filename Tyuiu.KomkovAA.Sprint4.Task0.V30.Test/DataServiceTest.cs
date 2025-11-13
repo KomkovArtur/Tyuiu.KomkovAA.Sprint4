9e42978b75c786de2d92c3e3c1716626e0956690
артур комков <stenplay33@gmail.com>
2025-11-13 14:57:58 +0500

    Add project files.

---
+++ Tyuiu.KomkovAA.Sprint4.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KomkovAA.Sprint4.Task0.V30;
+using Tyuiu.KomkovAA.Sprint4.Task0.V30.Lib;
+namespace Tyuiu.KomkovAA.Sprint4.Task0.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] ch = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            int res = 30;
+            int chres = ds.GetSumEvenArrEl(ch);
+            Assert.AreEqual(res, chres);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint4.Task0.V30/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KomkovAA.Sprint4.Task0.V30;
+using Tyuiu.KomkovAA.Sprint4.Task0.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] ch = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+        for (int i = 0; i < ch.Length; i++)
+        {
+            Console.WriteLine(ch[i]);
+        
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int chres = ds.GetSumEvenArrEl(ch);
+        Console.Write(chres);
+
+    }
+}
