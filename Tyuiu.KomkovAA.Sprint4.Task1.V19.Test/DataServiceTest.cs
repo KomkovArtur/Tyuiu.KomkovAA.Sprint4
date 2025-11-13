733da7cfc2e81a5571be22fbcc4c87f35762dabc
артур комков <stenplay33@gmail.com>
2025-11-13 15:58:55 +0500

    Task1

---
+++ Tyuiu.KomkovAA.Sprint4.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KomkovAA.Sprint4.Task1.V19.Lib;
+namespace Tyuiu.KomkovAA.Sprint4.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] mas = new int[12] { 6, 6, 9, 6, 9, 3, 8, 2, 9, 5, 7, 4 };
+            DataService ds = new DataService();
+            int ch = ds.Calculate(mas);
+            Assert.AreEqual(32, ch);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint4.Task1.V19/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KomkovAA.Sprint4.Task1.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите количество элементов массива");
+        int len = Convert.ToInt32(Console.ReadLine());
+        int[] mas = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            Console.WriteLine($"Введите элемент {i} массива");
+            mas[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int res = 0;
+        for (int i = 0;i < len; i++)
+        {
+            if (mas[i] % 2 == 0)
+            {
+                res += mas[i];
+            }
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < len; i++)
+        {
+            Console.WriteLine(mas[i]);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.Write(res);
+    }
+}
